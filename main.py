05e4ee2cec75d4a8e27a749e03694c6b08af72bf
Nikita <nikita.c04@mail.ru>
2021-12-22 17:57:34 +0300

    final

--- main.py
+++ main.py
@@ -2,28 +2,31 @@ import time
 
 
 def main():
-    min_barrier = int(input())
-    max_barrier = int(input())
+    min_barrier = int(input('Enter minimal barrier (l): '))
+    max_barrier = int(input('Enter maximum barrier (r): '))
     start_time = time.time()
     if min_barrier <= max_barrier:
         all_roots = []
         for i in range(min_barrier, max_barrier + 1):
-            max_range_dgt = 100000
             min_range_dgt = -100000 + i
+            max_range_dgt = 100000
             interval = [max_range_dgt, min_range_dgt]
             while interval[1] != max_range_dgt:
-                current_discriminant = discriminant_calc(interval[0], interval[1])
-                if current_discriminant is not None:
-                    all_roots.append(current_discriminant)
+                current_roots = equal_calc(interval[0], interval[1])
+                if current_roots is not None:
+                    all_roots.append(current_roots)
                 interval[0] -= 1
                 interval[1] += 1
-        print(len(all_roots))
-        print("--- %s seconds ---" % (time.time() - start_time))
+        if len(all_roots) > 200000:
+            print(-1)
+        else:
+            print(f'All roots: {all_roots}\nNumber of roots: {len(all_roots)}')
+        print(f'Lead time: {time.time() - start_time} seconds')
     else:
         print("min_barrier > max_barrier")
 
 
-def discriminant_calc(b_coef, c_coef):
+def equal_calc(b_coef, c_coef):
     discriminant = b_coef ** 2 - 4 * c_coef
     roots = [0, 0]
     if discriminant > 0:
